cf6b14c8f8cd9292b015094ad4aea554629aa7cc
AmyTroutman <amyrtroutman@gmail.com>
2020-07-14 21:25:25 -0500

    Removed unused bits

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,23 +7,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { StudentsComponent } from './students/students.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
-import { TeacherComponent } from './teacher/teacher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    StudentsComponent,
-    TeacherComponent
+  
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -32,10 +25,7 @@ import { TeacherComponent } from './teacher/teacher.component';
     ApiAuthorizationModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-        { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-        { path: 'students', component: StudentsComponent, canActivate: [AuthorizeGuard] },
-        { path: 'teachers', component: TeacherComponent, canActivate: [AuthorizeGuard] },
+   
     ])
   ],
   providers: [
