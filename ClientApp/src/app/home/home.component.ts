42187c06004f9c1e25b48155df915cefa12077a1
AmyTroutman <amyrtroutman@gmail.com>
2020-08-02 10:18:57 -0500

    messing with some home page views based on if logged in

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthorizeService } from 'src/api-authorization/authorize.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
 export class HomeComponent {
+  loggedIn = false;
+  constructor(private authorize: AuthorizeService) {
+    if (this.authorize.isAuthenticated()) { this.loggedIn = true; }
+   }
+
 }
