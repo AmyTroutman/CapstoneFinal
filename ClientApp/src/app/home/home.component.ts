b10927fa113c57da3e944e5d39d45ca954d106d3
AmyTroutman <amyrtroutman@gmail.com>
2020-08-13 20:57:26 -0500

    fixed unhappy ngoninit

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -3,20 +3,19 @@ import { AuthorizeService } from 'src/api-authorization/authorize.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public isAuthenticated: Observable<boolean>;
   public userName: Observable<string>;
 
 
   constructor(private authorizeService: AuthorizeService) {   }
 
-   ngOnInit() {
+  ngOnInit() {
     this.isAuthenticated = this.authorizeService.isAuthenticated();
     this.userName = this.authorizeService.getUser().pipe(map(u => u && u.name));
 
