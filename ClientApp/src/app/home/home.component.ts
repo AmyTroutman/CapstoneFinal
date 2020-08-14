73c9fa3d62f8a3b9e7418da3bc0e459fbd3d6ae7
AmyTroutman <amyrtroutman@gmail.com>
2020-08-13 20:49:40 -0500

    messing with username

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorizeService } from 'src/api-authorization/authorize.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -10,11 +11,15 @@ import { Observable } from 'rxjs';
 
 export class HomeComponent {
   public isAuthenticated: Observable<boolean>;
+  public userName: Observable<string>;
+
 
   constructor(private authorizeService: AuthorizeService) {   }
 
    ngOnInit() {
     this.isAuthenticated = this.authorizeService.isAuthenticated();
+    this.userName = this.authorizeService.getUser().pipe(map(u => u && u.name));
+
   }
 
 }
