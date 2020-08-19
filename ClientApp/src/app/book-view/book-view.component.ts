f68d41c997ec1bf310b154b1c3b329f817ec3b66
AmyTroutman <amyrtroutman@gmail.com>
2020-08-18 20:01:58 -0500

    EVERYTHING WORKS! Just need some confirmation modals.

--- ClientApp/src/app/book-view/book-view.component.ts
+++ ClientApp/src/app/book-view/book-view.component.ts
@@ -10,28 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BookViewComponent implements OnInit {
 
-  loading = true;
   books: IBook[];
   bookId: number;
   book: IBook = {id: 0, title: '', author: '', notes: '', series: '', type: '', userId: '', genre: '', status: ''};
-  options: string[];
-  types: string[];
 
   constructor(private bookService: BookService, private route: ActivatedRoute) { }
 
   async ngOnInit() {
     this.bookId = this.route.snapshot.params.id;
-   // this.books = await this.bookService.GetBooks();
+    this.books = await this.bookService.GetBooks();
     this.book = await this.bookService.GetBook(this.bookId);
     console.log(this.book);
-    // this.options = this.bookService.statuses;
-    // this.types = this.bookService.types;
   }
 
   async save(): Promise<void> {
     await this.bookService.UpdateBook(this.bookId, this.book);
     this.book = {id: this.bookId, title: '', author: '', notes: '', series: '', type: '', userId: '', genre: '', status: ''};
-    // this.books = await this.bookService.GetBooks();
+    this.books = await this.bookService.GetBooks();
   }
 
   async deleteBook(): Promise<void> {
--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthorizeService } from 'src/api-authorization/authorize.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { IBook } from '../ibook';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +13,21 @@ import { map } from 'rxjs/operators';
 export class HomeComponent implements OnInit {
   public isAuthenticated: Observable<boolean>;
   public userName: Observable<string>;
+  books: IBook[];
+  read: IBook[] = [];
+  reading: IBook[] = [];
+  buy: IBook[] = [];
 
 
-  constructor(private authorizeService: AuthorizeService) {   }
+  constructor(private authorizeService: AuthorizeService, private bookService: BookService) {   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.isAuthenticated = this.authorizeService.isAuthenticated();
     this.userName = this.authorizeService.getUser().pipe(map(u => u && u.name));
-
+    this.books = await this.bookService.GetBooks();
+    this.read = this.books.filter(b => b.status === 'Wanna Read');
+    this.reading = this.books.filter(b => b.status === 'Am Reading');
+    this.buy = this.books.filter(b => b.status === 'Need to Buy');
   }
 
 }
