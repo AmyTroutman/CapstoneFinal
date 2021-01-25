435f5a6b17b51b56d3cbe76bdba5765bcba5be2b
AmyTroutman <amyrtroutman@gmail.com>
2021-01-25 07:50:32 -0600

    linked home stylesheet, colored a, added borrowed to status list

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { BookService } from '../services/book.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
